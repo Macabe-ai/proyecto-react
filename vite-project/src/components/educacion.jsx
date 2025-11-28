d089efb28fef4aef23743c54aed2653e2a4fefe4
Miguel Ángel Castilla Bedoya <muzzta.castilla0624@gmail.com>
2025-11-28 10:06:25 -0500

    feat: componente Educacion dinamico con formación complementaria

--- vite-project/src/components/educacion.jsx
+++ vite-project/src/components/educacion.jsx
@@ -1,12 +1,23 @@
 export default function Educacion() {
+  const educacion = [
+    {
+      id: 1,
+      titulo: "Bachillerato",
+      institucion: "SENA Pedregal",
+      extra: "Media Técnica en Sistemas",
+      periodo: "2022-2024"
+    }
+  ];
+
   return (
     <section>
       <h3>Educación</h3>
       <ul>
-        <li>
-          <strong>Bachillerato</strong> -
-         <strong>Media Técnica</strong> - Sistema (Sena Pedregal) (2022-2024)
-        </li>
+        {educacion.map((edu) => (
+          <li key={edu.id}>
+            <strong>{edu.titulo}</strong> - {edu.extra} - {edu.institucion} ({edu.periodo})
+          </li>
+        ))}
       </ul>
     </section>
   );
