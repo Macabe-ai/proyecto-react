1ccfeef42418f187bef876e67cc3a4c434ebcfc4
Miguel Ángel Castilla Bedoya <muzzta.castilla0624@gmail.com>
2025-11-28 10:44:54 -0500

    feat: componente CabeceraCV ahora recibe props

--- vite-project/src/components/cabeceraCV.jsx
+++ vite-project/src/components/cabeceraCV.jsx
@@ -1,20 +1,8 @@
-export default function CabeceraCV() {
+export default function CabeceraCV({ nombre, profesion }) {
   return (
     <header className="cv-header">
-      <img
-        src="./public/img/perfil.jpg"
-        alt="Foto de perfil"
-        className="profile-pic"
-      />
-
-      <h1 className="cv-name">Miguel Ángel Castilla Bedoya - -</h1>
-      <h2 className="cv-title">Analista y Desarrollador de Software</h2>
-
-      <div className="cv-contact">
-        <p><strong>Teléfono:</strong> 324 3122312</p>
-        <p><strong>Correo:</strong> muzzta.castilla0624@gmail.com</p>
-        <p><strong>Ciudad:</strong> Medellín, Colombia</p>
-      </div>
+      <h1>{nombre}</h1>
+      <h2>{profesion}</h2>
     </header>
   );
 }
