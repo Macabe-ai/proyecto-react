9f093f2e00ded89c340d0dc1c6d001adf5ebb5b6
Miguel Ángel Castilla Bedoya <muzzta.castilla0624@gmail.com>
2025-11-28 09:40:31 -0500

    feat: componente Perfil con descripción personalizada

--- vite-project/src/components/perfil.jsx
+++ vite-project/src/components/perfil.jsx
@@ -3,7 +3,7 @@ export default function Perfil() {
     <section>
       <h3>Perfil</h3>
       <p>
-        Tecnologo en Analisis y Desarrollo de software con experiencia en desarrollo web y móvil, apasionado por la tecnología.
+        Tecnologo en Analísis y Desarrollo de software con experiencia en desarrollo web y móvil, apasionado por la tecnología.
       </p>
     </section>
   );
