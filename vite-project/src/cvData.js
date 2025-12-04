1f47088219190a9604a7e9d9633cea0b16139ed3
Miguel Ángel <muzzta.castilla0624@gmail.com>
2025-12-04 14:58:11 -0500

    feat: renderizado dinámico de tecnologías desde estado

---
+++ vite-project/src/cvData.js
@@ -0,0 +1,15 @@
+// src/cvData.js
+export const initialTecnologias = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Git"
+];
+
+export const perfilData = {
+  nombre: "Miguel Ángel",
+  resumen: "Aprendiz de Análisis y Desarrollo de Software con conocimientos en desarrollo web."
+};
+
+// Puedes añadir más datos (educación, experiencia) y exportarlos aquí.
